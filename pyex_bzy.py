9df1b447f286ad67dfb84cfeb81417e1577f0607
wangxichang <wangxichang@126.com>
2018-06-27 19:43:27 +0800

    Merge branch 'master' of https://github.com/WangXichang/pyexam
    # Conflicts:
    #	pyex_seg.py

--- pyex_bzy.py
+++ pyex_bzy.py
@@ -2,8 +2,10 @@
 
 def lk(df, low, high):
     print(df[['xx', 'lkpos']][(df.lkpos <= high) & (df.lkpos >= low)])
+    print(df[(df.lkpos <= high) & (df.lkpos >= low)].xx.apply(lambda  x: str(x).strip().ljust()))
 
 
 def wk(df, low, high):
     print(df[['xx', 'wkpos']][(df.wkpos <= high) & (df.wkpos >= low)])
+    print(df[(df.wkpos <= high) & (df.wkpos >= low)].xx)
 
