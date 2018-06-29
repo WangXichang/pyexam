ecada1b839c9d20d2b5d72bd931bebdf8a86f119
wangxichang <wangxichang@126.com>
2018-06-29 08:55:37 +0800

    Merge branch 'master' of https://github.com/WangXichang/pyexam
    # Conflicts:
    #	pyex_bzy.py add make_page

--- pyex_bzy.py
+++ pyex_bzy.py
@@ -23,6 +23,8 @@ class ZhiYuan:
         self.td16p1 = None
         self.td16p2 = None
         self.td17bk = None
+        self.td16zk = None
+        self.td17zk = None
         self.dflq = None
         # self.load_data()
 
@@ -35,94 +37,116 @@ class ZhiYuan:
                                   dtype={'xx': str,}, verbose=True)
         self.td16p1 = self.td16p1.fillna(0)
         self.td16p1.astype(dtype={'wkpos': int, 'lkpos': int})
-        self.td16p1.loc[:, 'xxh'] = self.td16p1.xx.apply(lambda x: x[0:4])
+        # self.td16p1.loc[:, 'xxh'] = self.td16p1.xx.apply(lambda x: x[0:4])
 
         self.td16p2 = pd.read_csv(self.path+'td2016pc2_sc.csv', sep=',',
                                   dtype={'xx': str}, verbose=True)
         self.td16p2 = self.td16p2.fillna(0)
         self.td16p2.astype(dtype={'wkpos': int, 'lkpos': int})
 
-        self.td16p2.loc[:, 'xxh'] = self.td16p2.xx.apply(lambda x: x[0:4])
+        # self.td16p2.loc[:, 'xxh'] = self.td16p2.xx.apply(lambda x: x[0:4])
 
         self.td17bk = pd.read_csv(self.path+'td2017bk_sc.csv', sep=',',
                                   dtype={'xx': str}, verbose=True)
         self.td17bk = self.td17bk.fillna(0)
         self.td17bk.astype(dtype={'wkpos': int, 'lkpos': int, 'xx': str})
-        self.td17bk.loc[:, 'xxh'] = self.td17bk.xx.apply(lambda x: str(x)[0:4])
+        # self.td17bk.loc[:, 'xxh'] = self.td17bk.xx.apply(lambda x: str(x)[0:4])
+
+        self.td16zk = pd.read_csv(self.path+'td2016zk_sc.csv', sep=',',
+                                  dtype={'xx': str}, verbose=True)
+        self.td17zk = pd.read_csv(self.path+'td2017zk_sc.csv', sep=',',
+                                  dtype={'xx': str}, verbose=True)
 
         if os.path.isfile('f:/studies/lqdata/2015pc2lqk.csv'):
             self.dflq = pd.read_csv('f:/studies/lqdata/2015pc2lqk.csv', sep='\t', low_memory=False)
 
-    def somexx(self, filter='医学', kl='wk'):
-        print('2016p1---')
-        print(self.td16p1[self.td16p1.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
-              sort_values(by=('lkpos' if kl == 'lk' else 'wkpos')))
-        print('2016p2---')
-        print(self.td16p2[self.td16p2.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
-              sort_values(by='lkpos' if kl == 'lk' else 'wkpos'))
-        print('2017bk---')
-        print(self.td17bk[self.td17bk.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
-              sort_values(by='lkpos' if kl == 'lk' else 'wkpos'))
-
-    def lk(self, low, high, filterlist=('',)):
-        if (not isinstance(filterlist, list)) & (not isinstance(filterlist, tuple)):
-            print('filterstr is not list or tuple!')
-            return
-        filterfun = self.filterclosed(filterlist)
-        print('2016pc1---')
-        print(self.td16p1[['xx', 'lkpos']][(self.td16p1.lkpos <= high) & (self.td16p1.lkpos >= low) &
-              # self.td16p1.xx.apply(lambda x: filter in str(x))
-              self.td16p1.xx.apply(filterfun)
-              ])
-        print('2016pc2---')
-        print(self.td16p2[['xx', 'lkpos']][(self.td16p2.lkpos <= high) & (self.td16p2.lkpos >= low) &
-              #self.td16p2.xx.apply(lambda x: filter in str(x))
-              self.td16p2.xx.apply(lambda x: filterfun(str(x)))
-              ])
-        print('2017---')
-        print(self.td17bk[['xx', 'lkpos']][(self.td17bk.lkpos <= high) & (self.td17bk.lkpos >= low) &
-              # self.td17bk.xx.apply(lambda x: filter in str(x))
-              self.td17bk.xx.apply(lambda x: filterfun(str(x)))
-              ])
-
-    def findxx(self, low, high, filterlist=('',), kl='wk'):
-        posfield = 'wkpos' if kl == 'wk' else 'lkpos'
-        print('2016pc1---')
-        df1 = self.get_df_from_pos(self.td16p1, lowpos=low, highpos=high, posfield=posfield,
-                                   filterlist=filterlist, kl=kl)
-        df1.loc[:, 'xxh'] = df1.xx.apply(lambda x: x[0:4])
-        # print(pt.df_to_table(df1))
-        # print(make_table(df1, title='2016pc1'))
-        # print(df1)
-        print('2016pc2---')
-        df2 = self.get_df_from_pos(self.td16p2, lowpos=low, highpos=high, posfield=posfield,
-                                   filterlist=filterlist, kl=kl)
-        df2.loc[:, 'xxh'] = df2.xx.apply(lambda x: x[0:4])
-        # print(make_table(df2, title='2016pc2'))
-        # print(df2)
-        print('2017---')
-        df3 = self.get_df_from_pos(self.td17bk, lowpos=low, highpos=high, posfield=posfield,
-                                   filterlist=filterlist, kl=kl)
-        df3.loc[:, 'xxh'] = df3.xx.apply(lambda x: str(x)[0:4])
-        # print(make_table(df3, title='2017bk'))
-        # print(df3)
-        if kl == 'lk':
-            df1 = df1.rename(columns={'lkjh': 'lkjh16', 'lkpos': 'lkpos16'})
-            df2 = df2.rename(columns={'lkjh': 'lkjh16p2', 'lkpos': 'lkpos16p2'})
-            outfields = ['xx', 'lkjh', 'lkjh16', 'lkjh16p2', 'lkpos', 'lkpos16', 'lkpos16p2']
+    def somexx(self, filter='医学', kl='wk', cc='bk'):
+        if cc == 'bk':
+            print('2016p1---')
+            print(self.td16p1[self.td16p1.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
+                  sort_values(by=('lkpos' if kl == 'lk' else 'wkpos')))
+            print('2016p2---')
+            print(self.td16p2[self.td16p2.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
+                  sort_values(by='lkpos' if kl == 'lk' else 'wkpos'))
+            print('2017bk---')
+            print(self.td17bk[self.td17bk.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
+                  sort_values(by='lkpos' if kl == 'lk' else 'wkpos'))
         else:
-            df1 = df1.rename(columns={'wkjh': 'wkjh16', 'wkpos': 'wkpos16'})
-            df2 = df2.rename(columns={'wkjh': 'wkjh16p2', 'wkpos': 'wkpos16p2'})
-            outfields = ['xx', 'wkjh', 'wkjh16', 'wkjh16p2', 'wkpos', 'wkpos16', 'wkpos16p2']
-        dfmerge = pd.merge(df3, df1, on='xx', how='outer')
-        dfmerge = pd.merge(dfmerge, df2, on='xx', how='outer')[outfields]
-        # dfmerge = pd.merge(dfmerge, self.td17bk[['xxh', 'xx']], on='xxh')
-        # dfmerge = dfmerge[['xx'] + outfields]
-        dfmerge = dfmerge.fillna('0')
-        if kl == 'lk':
-            dfmerge = dfmerge.astype(dtype={'lkpos': int, 'lkpos16': int, 'lkpos16p2': int,
-                                            'lkjh': int, 'lkjh16': int, 'lkjh16p2': int
-                                            }, errors='ignore')
+            print('2016zk---')
+            print(self.td16zk[self.td16zk.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
+                  sort_values(by=('lkpos' if kl == 'lk' else 'wkpos')))
+            print('2017zk---')
+            print(self.td17zk[self.td17zk.xx.apply(lambda x: filter in str(x))][['xx', 'wkpos', 'lkpos']].
+                  sort_values(by='lkpos' if kl == 'lk' else 'wkpos'))
+
+    def findxx(self, low, high, filterlist=('',), kl='wk', cc='bk'):
+        posfield = 'wkpos' if kl == 'wk' else 'lkpos'
+        if cc == 'bk':
+            print('2016pc1---')
+            df1 = self.get_df_from_pos(self.td16p1, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            # df1.loc[:, 'xxh'] = df1.xx.apply(lambda x: x[0:4])
+            # print(pt.df_to_table(df1))
+            # print(make_table(df1, title='2016pc1'))
+            # print(df1)
+            print('2016pc2---')
+            df2 = self.get_df_from_pos(self.td16p2, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            # df2.loc[:, 'xxh'] = df2.xx.apply(lambda x: x[0:4])
+            # print(make_table(df2, title='2016pc2'))
+            # print(df2)
+            print('2017---')
+            df3 = self.get_df_from_pos(self.td17bk, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            df3.loc[:, 'xxh'] = df3.xx.apply(lambda x: str(x)[0:4])
+            # print(make_table(df3, title='2017bk'))
+            # print(df3)
+            if kl == 'lk':
+                df1 = df1.rename(columns={'lkjh': 'lkjh16', 'lkpos': 'lkpos16'})
+                df2 = df2.rename(columns={'lkjh': 'lkjh16p2', 'lkpos': 'lkpos16p2'})
+                outfields = ['xx', 'lkjh', 'lkjh16', 'lkjh16p2', 'lkpos', 'lkpos16', 'lkpos16p2']
+            else:
+                df1 = df1.rename(columns={'wkjh': 'wkjh16', 'wkpos': 'wkpos16'})
+                df2 = df2.rename(columns={'wkjh': 'wkjh16p2', 'wkpos': 'wkpos16p2'})
+                outfields = ['xx', 'wkjh', 'wkjh16', 'wkjh16p2', 'wkpos', 'wkpos16', 'wkpos16p2']
+            dfmerge = pd.merge(df3, df1, on='xx', how='outer')
+            dfmerge = pd.merge(dfmerge, df2, on='xx', how='outer')[outfields]
+            # dfmerge = pd.merge(dfmerge, self.td17bk[['xxh', 'xx']], on='xxh')
+            # dfmerge = dfmerge[['xx'] + outfields]
+            dfmerge = dfmerge.fillna('0')
+            if kl == 'lk':
+                dfmerge = dfmerge.astype(dtype={'lkpos': int, 'lkpos16': int, 'lkpos16p2': int,
+                                                'lkjh': int, 'lkjh16': int, 'lkjh16p2': int
+                                                }, errors='ignore')
+            else:
+                dfmerge = dfmerge.astype(dtype={'wkpos': int, 'wkpos16': int, 'wkpos16p2': int,
+                                                'wkjh': int, 'wkjh16': int, 'wkjh16p2': int
+                                                }, errors='ignore')
+        else:
+            print('2016zk---')
+            df1 = self.get_df_from_pos(self.td16p1, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            print('2017zk---')
+            df2 = self.get_df_from_pos(self.td17bk, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            if kl == 'lk':
+                df1 = df1.rename(columns={'lkjh': 'lkjh16', 'lkpos': 'lkpos16'})
+                df2 = df2.rename(columns={'lkjh': 'lkjh16p2', 'lkpos': 'lkpos16p2'})
+                outfields = ['xx', 'lkjh', 'lkjh16', 'lkjh16p2', 'lkpos', 'lkpos16', 'lkpos16p2']
+            else:
+                df1 = df1.rename(columns={'wkjh': 'wkjh16', 'wkpos': 'wkpos16'})
+                df2 = df2.rename(columns={'wkjh': 'wkjh16p2', 'wkpos': 'wkpos16p2'})
+                outfields = ['xx', 'wkjh', 'wkjh16', 'wkjh16p2', 'wkpos', 'wkpos16', 'wkpos16p2']
+            dfmerge = pd.merge(df2, df1, on='xx', how='outer')[outfields]
+            dfmerge = dfmerge.fillna('0')
+            if kl == 'lk':
+                dfmerge = dfmerge.astype(dtype={'lkpos': int, 'lkpos16': int, 'lkpos16p2': int,
+                                                'lkjh': int, 'lkjh16': int, 'lkjh16p2': int
+                                                }, errors='ignore')
+            else:
+                dfmerge = dfmerge.astype(dtype={'wkpos': int, 'wkpos16': int, 'wkpos16p2': int,
+                                                'wkjh': int, 'wkjh16': int, 'wkjh16p2': int
+                                                }, errors='ignore')
         print(make_table(dfmerge))
         return dfmerge
 
