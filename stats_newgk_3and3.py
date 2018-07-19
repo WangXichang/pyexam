fe03299af93d7bea1a16e9e69ba143df4bc161eb
wang xichang <wangxichang@126.com>
2018-07-19 08:29:49 +0800

    pyex_seg: add segstart, seglist, seglistuse pyex_bzy: findxx, somexx, findzy using closedfun   make_page ok,  bk call closed baokao in bzy stats_newgk

--- stats_newgk_3and3.py
+++ stats_newgk_3and3.py
@@ -25,13 +25,24 @@ def get_cjfun():
             yeari = int(year) - 15
         else:
             yeari = 0
-        return datawk[yeari] if kl=='wk' else datalk[yeari]
-
+        flist = ['dl', 'ls', 'zz'] if kl == 'wk' else ['wl', 'hx', 'sw']
+        df = datawk[yeari] if kl == 'wk' else datalk[yeari]
+        for fs in flist:
+            if fs not in ['wl', 'sw']:
+                df.loc[:, fs+'n'] = df[fs].apply(lambda x: round(x))
+            elif fs == 'wl':
+                df.loc[:, fs+'n'] = df[fs].apply(lambda x: round(x/11*10))
+            elif fs == 'sw':
+                df.loc[:, fs+'n'] = df[fs].apply(lambda x: round(x/9*10))
+        df.loc[:, 'kl'] = df['ksh'].apply(lambda x: x[4:10])
+        df = df.drop('ksh', axis=1)
+        return df
     return get_cj
 
 def disp(df, kl='wk', minscore=500, maxscore=600):
     flist = ['yw', 'sx', 'wy', 'dl', 'ls', 'zz'] if kl=='wk' else \
         ['yw', 'sx', 'wy', 'wl', 'hx', 'sw']
+    flist = [fs+'n' for fs in flist]
     tdf = df[(df.zf <= maxscore) & (df.zf >= minscore)]
     if 'sg' in dir():
         pb.reload(sg)
@@ -44,18 +55,28 @@ def disp(df, kl='wk', minscore=500, maxscore=600):
     print(tdf[flist].describe())
     return dfo
 
-def desc(df, kl='wk', minscore=300, maxscore=400):
-    flist = ['yw', 'sx', 'wy', 'dl', 'ls', 'zz'] if kl=='wk' else \
+def desc(df, kl='wk', minscore=300, maxscore=400, step=50):
+    flist = ['yw', 'sx', 'wy', 'dl', 'ls', 'zz'] if kl == 'wk' else \
         ['yw', 'sx', 'wy', 'wl', 'hx', 'sw']
-    dr = pd.DataFrame({'scope': []}.update({fs+'_std': [] for fs in flist}))
-    for fs in flist:
-        dr2 = dict()
-        dr2.update({'scope': [str(minscore)+'-'+str(maxscore)]})
-        if fs in df.columns:
-            tstd = std(df[fs][(df.zf>=minscore) & (df.zf<=maxscore)])
-            tmean = mean(df[fs][(df.zf>=minscore) & (df.zf<=maxscore)])
-            print('{}: std = {}, mean = {}'.format(fs,tstd, tmean))
-            dr2.update({fs+'_std': [tstd]})
-        dr = dr.append(pd.DataFrame(dr2))
-    print(dr)
-    return dr
+    flist = [fs+'n' if fs not in ['yw', 'sx', 'wy'] else fs for fs in flist]
+    d1 = {'scope': []}
+    d1.update({fs+'_std': [] for fs in flist})
+    dstd = pd.DataFrame(d1)
+    dcor = pd.DataFrame(d1)
+    for st in range(minscore, maxscore, step):
+        dt1 = dict()
+        dt1.update({'scope': [str(st) + '-' + str(st+step)]})
+        dt2 = dict()
+        dt2.update({'scope': [str(st) + '-' + str(st+step)]})
+        for fs in flist:
+            if fs in df.columns:
+                dftemp = df[(df.zf >= st) & (df.zf <= st+step)]
+                tstd = std(dftemp[fs])
+                tcor = round(stt.pearsonr(dftemp[fs], dftemp['zf'])[0], 4)
+                dt1.update({fs+'_std': [tstd]})
+                dt2.update({fs+'_cor': [tcor]})
+        dstd = dstd.append(pd.DataFrame(dt1))
+        dcor = dcor.append(pd.DataFrame(dt2))
+    print(dstd[['scope'] + [fs+'_std' for fs in flist]])
+    print(dcor[['scope'] + [fs+'_cor' for fs in flist]])
+    return dstd
