9510701d3ca1b16a80f3b8defa4e0c6a9045511b
wangxichang <wangxichang@126.com>
2018-10-26 09:52:02 +0800

    plot mode=out for plt and level model new report in level_model

---
+++ ChangeScore_lbc.py
@@ -0,0 +1,216 @@
+import pandas as pd
+import os
+import pyex_seg as pg
+class ChangeScore(object):
+    def __init__(self):
+        self.__segFields = []
+        self._generate_origi_score_list_way='near'
+        self._input_dataframe=None
+        self._input_origi_score_list_dataframe=None
+        self._output_stand_score_list_dataframe=None
+        self._segOrigiMax={'yw':150,'sx':150,'wy':150,'wl':110,'sw':90,'hx':100,'dl':100,'ls':100,'zz':100}
+        self._segOrigiMin={'yw':0,'sx':0,'wy':0,'wl':0,'sw':0,'hx':0,'dl':0,'ls':0,'zz':0}
+        self._segObjMax=100
+        self._segObjMin=20
+        self._segNumber=8
+        self._percentList=[0.03, 0.07, 0.16, 0.24, 0.24, 0.16, 0.07, 0.03]
+        self._segStandList=[20, 30, 40, 50, 60, 70, 80, 90, 100]
+        self._seg_origi_standlist={} # 原始分数点列表
+        self.__output_segstandList_dataframe = None  # 分数
+        self._score_dict={}
+    @property
+    def output_data(self):
+        return self._output_stand_score_list_dataframe
+
+    def set_parameters(self,segObjMax=None,segObjMin=None,segNumber=None,percentList=None,segStandList=None,generate_origi_score_list_way=None):
+        if isinstance(segObjMax, int):
+            self._segObjMax=segObjMax
+        if isinstance(segObjMin, int):
+            self._segObjMin=segObjMin
+        if isinstance(segNumber, int):
+            self._segNumber=segNumber
+        self._generate_origi_score_list_way=generate_origi_score_list_way
+    # 设置一分一段表的DF
+    def set_data_yfyd(self,df, field_list=None):
+        self._input_dataframe = df
+        self.__segFields= field_list
+
+    # 设置要转换的原始分数的DF
+    def set__input_origi_score_list_dataframe(self,df):
+        self._input_origi_score_list_dataframe = df
+
+    # 设置按比例求出的分数点
+    def set_score_dict(self):
+        for f in self.__segFields:
+            temp_origi_standlist=self._seg_origi_standlist.get(f)
+            # print(temp_origi_standlist)
+            temp_dict={}
+            for i in range(len(temp_origi_standlist)):
+                temp_dict[temp_origi_standlist[i]]=self._segStandList[i]
+            self._score_dict[f]=temp_dict
+    # 四舍五入
+    def my_round(self,number, point):
+        temp = 10 ** point
+        temp_int = int(number * temp)
+        diff = abs(number * temp - temp_int)
+        if number >= 0:
+            result = temp_int / temp if diff < 0.5 else (temp_int + 1) / temp
+        else:
+            result = temp_int / temp if diff < 0.5 else (temp_int - 1) / temp
+        return result
+    # 转换分数
+    def getEveryScore(self,x,f,which_point):
+        temp_dict=self._score_dict.get(f)
+        temp_seg_origi_standlist=self._seg_origi_standlist.get(f)
+        if x in temp_dict:
+            return temp_dict.get(x)  # 如果分数正好在分段点上，直接返回对应分数
+        previous=0
+        next=0
+        not_found=True
+        for i in range(len(temp_seg_origi_standlist)):
+            if not_found:
+                next=temp_seg_origi_standlist[i]
+                if x<next:
+                    not_found=False
+                    previous=temp_seg_origi_standlist[i-1]
+            else:
+                break
+        previous_value=self._score_dict.get(f).get(previous)
+        next_value=self._score_dict.get(f).get(next)
+
+        return self.my_round((x-previous)*(next_value-previous_value)/(next-previous)+previous_value,which_point)
+
+    # 转换分数
+    def generate_stand_score(self,which_point):
+        self.set_score_dict()
+        self._output_stand_score_list_dataframe = self._input_origi_score_list_dataframe.copy()
+        for f in self.__segFields:
+            self._output_stand_score_list_dataframe[f+'_stand']=self._output_stand_score_list_dataframe[f].apply(self.getEveryScore,args=(f,which_point))
+        print(self._output_stand_score_list_dataframe)
+
+    # def set_origi_standList(self,stand_list):
+    #     self._seg_origi_standlist = stand_list
+
+    # 根据比例划分找出对应的原始分数
+    def generate_OrigiScoreList(self):
+        cussum = pd.Series(self._percentList).cumsum()
+        if self._generate_origi_score_list_way.lower()=='near':
+            for f in self.__segFields:
+                tempresult=[]
+                # tempresult.append(self._segOrigiMin.get(f))# 最小的放进来  段点是0-150 或者0-100的情况
+                tempresult.append(self._input_origi_score_list_dataframe[f].min())
+
+                for i in range(len(cussum)-1):
+                    tempLess=self._input_dataframe[self._input_dataframe[f+'_percent']<=cussum[i]]
+                    tempLessPoint=tempLess[(tempLess[f+'_percent']==tempLess[f+'_percent'].max()) & (tempLess[f+'_count']!=0)]
+
+                    tempMore=self._input_dataframe[self._input_dataframe[f+'_percent']>cussum[i]]
+                    tempMorePoint=tempMore[(tempMore[f+'_percent']==tempMore[f+'_percent'].min()) &  (tempMore[f+'_count']!=0)]
+
+                    if ( tempMorePoint[f+'_percent'].values[0]-cussum[i] )<(cussum[i]- tempLessPoint[f+'_percent'].values[0]) :
+                        tempresult.append(tempMorePoint['seg'].values[0])
+                    if ( tempMorePoint[f+'_percent'].values[0]-cussum[i] )> (cussum[i]- tempLessPoint[f+'_percent'].values[0]) :
+                        tempresult.append(tempLessPoint['seg'].values[0])
+                    if ( tempMorePoint[f+'_percent'].values[0]-cussum[i] )== (cussum[i]- tempLessPoint[f+'_percent'].values[0]) :
+                        tempresult.append(tempMorePoint['seg'].values[0])
+                tempresult.append(self._input_origi_score_list_dataframe[f].max())
+                print(tempresult)
+                self._seg_origi_standlist[f] =tempresult
+
+        if self._generate_origi_score_list_way.lower() == 'the_biggest_smaller_than_or_equal':
+            for f in self.__segFields:
+                tempresult = []
+                # tempresult.append(self._segOrigiMin.get(f))# 最小的放进来  段点是0-150 或者0-100的情况
+                tempresult.append(self._input_origi_score_list_dataframe[f].min())
+
+                for i in range(len(cussum) - 1):
+                    tempLess = self._input_dataframe[self._input_dataframe[f + '_percent'] <= cussum[i]]
+                    tempLessPoint = tempLess[
+                        (tempLess[f + '_percent'] == tempLess[f + '_percent'].max()) & (tempLess[f + '_count'] != 0)]
+
+                    tempresult.append(tempLessPoint['seg'].values[0])
+                tempresult.append(self._input_origi_score_list_dataframe[f].max())
+                print(tempresult)
+                self._seg_origi_standlist[f] = tempresult
+        if self._generate_origi_score_list_way.lower() == 'the_smallest_bigger_than_or_equal':
+            for f in self.__segFields:
+                tempresult = []
+                # tempresult.append(self._segOrigiMin.get(f))# 最小的放进来  段点是0-150 或者0-100的情况
+                tempresult.append(self._input_origi_score_list_dataframe[f].min())
+
+                for i in range(len(cussum) - 1):
+                    tempMore = self._input_dataframe[self._input_dataframe[f+'_percent'] > cussum[i]]
+                    tempMorePoint = tempMore[
+                        (tempMore[f + '_percent'] == tempMore[f + '_percent'].min()) & (tempMore[f + '_count'] != 0)]
+
+                    tempresult.append(tempMorePoint['seg'].values[0])
+                tempresult.append(self._input_origi_score_list_dataframe[f].max())
+                print(tempresult)
+                self._seg_origi_standlist[f] = tempresult
+
+
+
+
+def with_open(file_path):
+    try:
+        if not os.path.exists(file_path):
+            print(file_path + "不存在")
+            return
+        with open(file_path, encoding='ANSI') as f:
+            df = pd.read_csv(f)
+            return df
+    except Exception as e:
+        print(e)
+
+def outputresult(year,kl,which_point,change_way):
+    expdf = with_open("D://pythontest//"+year+kl+".txt")
+    seg = pg.SegTable()
+    flist=[]
+    if kl=='wz':
+        flist=['yw', 'sx', 'wy', 'dl', 'zz', 'ls']
+    if kl=='lz':
+        flist = ['yw','sx','wy','wl','hx','sw']
+    seg.set_data(expdf, flist)
+    if kl == 'wz':
+        seg.roundall(['dl', 'zz', 'ls'])
+    print(seg.input_data)
+    seg.set_parameters(segstep=1, segmax=150, segmin=0, segsort='ascending', usealldata=True, display=True)
+    seg.run()
+    seg.output_data.to_csv("D://pythontest//out"+year+kl+"_yfyd.csv",
+                           index=False, header=True, mode='w')
+    # output_data=with_open("D://pythontest//2015wz.csv")
+    changeScore = ChangeScore()
+    changeScore.set_data_yfyd(seg.output_data, flist)#设置一分一段表
+    changeScore.set__input_origi_score_list_dataframe(seg.input_data) #设置需转换的原始分数表
+    changeScore.set_parameters(generate_origi_score_list_way=change_way) #设置比例分数生成方式
+    # the_biggest_smaller_than_or_equal
+    # the_smallest_bigger_than_or_equal
+    # near
+
+    changeScore.generate_OrigiScoreList()
+    changeScore.generate_stand_score(which_point)
+    changeScore.output_data.to_csv("D://pythontest//out"+year+kl+"_stand.csv",
+                                   index=False, header=True, mode='w')
+if __name__ == '__main__':
+    # expdf = with_open("D://pythontest//2015wzfkmcj.txt")
+    # seg = pg.SegTable()
+    #
+    # seg.set_data(expdf, ['yw','sx','wy','dl','zz','ls'])
+    # seg.roundall(['dl','zz','ls'])
+    # print(seg.input_data)
+    # seg.set_parameters(segstep=1, segmax=150, segmin=0, segsort='ascending', segalldata=True, display=True)
+    # seg.run()
+    # seg.output_data.to_csv("D://pythontest//2015wz.csv",
+    #                        index=False, header=True, mode='w')
+    # # output_data=with_open("D://pythontest//2015wz.csv")
+    # changeScore = ChangeScore()
+    # changeScore.set_data(seg.output_data,['yw','sx','wy','dl','zz','ls'])
+    #
+    # changeScore.generate_OrigiScoreList()
+    # changeScore.set__input_origi_score_list_dataframe(seg.input_data)
+    # changeScore.generate_stand_score()
+    # changeScore.output_data.to_csv("D://pythontest//2015wzstand.csv",
+    #                        index=False, header=True, mode='w')
+
+    #   测试
+    outputresult('2015','lz',0,'near')
---
+++ pyex_bzy0702.py
@@ -0,0 +1,263 @@
+﻿# -*- utf -*-
+
+import os
+import pandas as pd
+from prettytable import PrettyTable as Ptt
+
+
+def getzy():
+    loc_dell = 'd:/work/data/lq/'
+    loc_off = 'f:/studies/lqdata/'
+    loc_suface = 'c:/users/wangxichang/zydata/'
+    loc_lq = 'd:/zy/'
+    loc_list = [loc_suface, loc_dell, loc_lq, loc_off]
+    
+    zy = Finder()
+    for p in loc_list:
+        if os.path.isfile(p+'td2017bk_sc.csv'):
+            zy.set_datapath(p)
+            return zy
+    
+    print('error: no valid path assigned!')
+    return None
+
+
+class Finder:
+    def __init__(self):
+        self.path = 'f:/studies/lqdata/'
+        self.td16p1 = None
+        self.td16p2 = None
+        self.td17bk = None
+        self.td16zk = None
+        self.td17zk = None
+        self.dflq = None
+
+    def set_datapath(self, path):
+        self.path = path
+        self.load_data()
+
+    def load_data(self):
+        self.td16p1 = pd.read_csv(self.path+'td2016pc1_sc.csv', sep=',',
+                                  dtype={'xx': str}, verbose=True)
+        self.td16p1 = self.td16p1.fillna(0)
+        self.td16p1.astype(dtype={'wkpos': int, 'lkpos': int})
+
+        self.td16p2 = pd.read_csv(self.path+'td2016pc2_sc.csv', sep=',',
+                                  dtype={'xx': str}, verbose=True)
+        self.td16p2 = self.td16p2.fillna(0)
+        self.td16p2.astype(dtype={'wkpos': int, 'lkpos': int})
+
+        self.td17bk = pd.read_csv(self.path+'td2017bk_sc.csv', sep=',',
+                                  dtype={'xx': str}, verbose=True)
+        self.td17bk = self.td17bk.fillna(0)
+        self.td17bk.astype(dtype={'wkpos': int, 'lkpos': int, 'xx': str})
+
+        self.td16zk = pd.read_csv(self.path+'td2016zk_sc.csv', sep=',',
+                                  dtype={'xx': str}, verbose=True)
+        self.td17zk = pd.read_csv(self.path+'td2017zk_sc.csv', sep=',',
+                                  dtype={'xx': str}, verbose=True)
+
+        if os.path.isfile(self.path+'2015pc2lqk.csv'):
+            self.dflq = pd.read_csv(self.path+'2015pc2lqk.csv', sep='\t', low_memory=False)
+
+    def somexx(self, xxsubstr=('医学',), kl='wk', cc='bk'):
+        ffun = closed_filter(xxsubstr)
+        if ffun is False:
+            return
+        df1, df2, df3 = None, None, None
+        outfields = ['xx', 'wkpos'] if kl == 'wk' else ['xx', 'lkpos']
+        sortfield = 'lkpos' if kl == 'lk' else 'wkpos'
+        if cc == 'bk':
+            # print('2016p1---')
+            df1 = self.td16p1[self.td16p1.xx.apply(ffun)][outfields].\
+                  sort_values(by=sortfield).astype({sortfield: int})
+            print(make_page(df1, '2016p1'))
+            # print('2016p2---')
+            df2 = self.td16p2[self.td16p2.xx.apply(ffun)][outfields].\
+                  sort_values(by=sortfield).astype({sortfield: int})
+            print(make_page(df2, '2016p2'))
+            # print('2017bk---')
+            df3 = self.td17bk[self.td17bk.xx.apply(ffun)][outfields].\
+                  sort_values(by=sortfield).astype({sortfield: int})
+            print(make_page(df3, '2017bk', align={'xx': 'l'}))
+        else:
+            # print('2016zk---')
+            df1 = self.td16zk[self.td16zk.xx.apply(ffun)][outfields].\
+                  sort_values(by=(sortfield)).astype({sortfield: int})
+            print(make_page(df1, title='2016zk', align={'xx': 'l'}))
+            # print('2017zk---')
+            df2 = self.td17zk[self.td17zk.xx.apply(ffun)][outfields].\
+                  sort_values(by=sortfield).astype({sortfield: int})
+            print(make_page(df2, title='2017zk', align={'xx': 'l'}))
+        return  # df1, df2, df3
+
+    def findxx(self, low, high, filterlist=('',), kl='wk', cc='bk', align={}):
+        posfield = 'wkpos' if kl == 'wk' else 'lkpos'
+        if cc == 'bk':
+            # print('2016pc1---')
+            df1 = self.get_df_from_pos(self.td16p1, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            # print('2016pc2---')
+            df2 = self.get_df_from_pos(self.td16p2, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            # print('2017---')
+            df3 = self.get_df_from_pos(self.td17bk, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            df3.loc[:, 'xxh'] = df3.xx.apply(lambda x: str(x)[0:4])
+            if kl == 'lk':
+                df1 = df1.rename(columns={'lkjh': 'lkjh16', 'lkpos': 'lkpos16'})
+                df2 = df2.rename(columns={'lkjh': 'lkjh16p2', 'lkpos': 'lkpos16p2'})
+                df3 = df3.rename(columns={'lkjh': 'lkjh17', 'lkpos': 'lkpos17'})
+                outfields = ['xx', 'lkjh17', 'lkjh16', 'lkjh16p2', 'lkpos17', 'lkpos16', 'lkpos16p2']
+            else:
+                df1 = df1.rename(columns={'wkjh': 'wkjh16', 'wkpos': 'wkpos16'})
+                df2 = df2.rename(columns={'wkjh': 'wkjh16p2', 'wkpos': 'wkpos16p2'})
+                df3 = df3.rename(columns={'wkjh': 'wkjh17', 'wkpos': 'wkpos17'})
+                outfields = ['xx', 'wkjh17', 'wkjh16', 'wkjh16p2', 'wkpos17', 'wkpos16', 'wkpos16p2']
+            dfmerge = pd.merge(df3, df1, on='xx', how='outer')
+            dfmerge = pd.merge(dfmerge, df2, on='xx', how='outer')[outfields]
+            dfmerge = dfmerge.fillna('0')
+            if kl == 'lk':
+                dfmerge = dfmerge.astype(dtype={'lkpos17': int, 'lkpos16': int, 'lkpos16p2': int,
+                                                'lkjh17': int, 'lkjh16': int, 'lkjh16p2': int
+                                                }, errors='ignore')
+            else:
+                dfmerge = dfmerge.astype(dtype={'wkpos17': int, 'wkpos16': int, 'wkpos16p2': int,
+                                                'wkjh17': int, 'wkjh16': int, 'wkjh16p2': int
+                                                }, errors='ignore')
+        else:
+            # print('2016zk---')
+            df1 = self.get_df_from_pos(self.td16zk, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            # print('2017zk---')
+            df2 = self.get_df_from_pos(self.td17zk, lowpos=low, highpos=high, posfield=posfield,
+                                       filterlist=filterlist, kl=kl)
+            if kl == 'lk':
+                df1 = df1.rename(columns={'lkjh': 'lkjh16', 'lkpos': 'lkpos16'})
+                df2 = df2.rename(columns={'lkjh': 'lkjh17', 'lkpos': 'lkpos17'})
+                outfields = ['xx', 'lkjh16', 'lkjh17', 'lkpos16', 'lkpos17']
+            else:
+                df1 = df1.rename(columns={'wkjh': 'wkjh16', 'wkpos': 'wkpos16'})
+                df2 = df2.rename(columns={'wkjh': 'wkjh17', 'wkpos': 'wkpos17'})
+                outfields = ['xx', 'wkjh16', 'wkjh17', 'wkpos16', 'wkpos17']
+            dfmerge = pd.merge(df2, df1, on='xx', how='outer')[outfields]
+            dfmerge = dfmerge.fillna('0')
+            if kl == 'lk':
+                dfmerge = dfmerge.astype(dtype={'lkpos16': int, 'lkpos17': int,
+                                                'lkjh16': int, 'lkjh17': int
+                                                }, errors='ignore')
+            else:
+                dfmerge = dfmerge.astype(dtype={'wkpos16': int, 'wkpos17': int,
+                                                'wkjh16': int, 'wkjh17': int
+                                                }, errors='ignore')
+        print(make_page(dfmerge, title='16-17：'+ cc, align=align))
+        return  dfmerge
+
+    def findzy(self, lowpos, highpos, zyfilterlist, xxfilterlist, kl='lk'):
+        if self.dflq is None:
+            return pd.DataFrame()
+        kldm = 1 if kl == 'wk' else 5
+        filterfunzy = closed_filter(zyfilterlist)
+        filterfunxx = closed_filter(xxfilterlist)
+        df = self.dflq[self.dflq.ZYMC.apply(filterfunzy) & (self.dflq.YXMC.apply(filterfunxx)) &
+                       (self.dflq.WC >= lowpos) & (self.dflq.WC <= highpos) & (self.dflq.KLDM == kldm)].\
+            groupby(['YXDH', 'ZYDH'])[['WC', 'YXMC', 'ZYMC']].max()
+        if len(df) == 0:
+            print(' no result!')
+            return
+        print(make_page(df.sort_values('WC'),
+                        title=''.join(zyfilterlist) + '(' + kl + ')',
+                        align={'YXMC': 'l', 'ZYMC': 'l'}))
+        return  # df
+
+    def get_df_from_pos(self, df, lowpos, highpos, posfield, filterlist, kl):
+        jh = 'wkjh' if kl == 'wk' else 'lkjh'
+        filterfun = closed_filter(filterlist)
+        return df[['xx', jh, posfield]][(df[posfield] <= highpos) &
+                                        (df[posfield] >= lowpos) &
+                                        df.xx.apply(filterfun)].sort_values(by=posfield)
+
+def closed_filter(substr_list):
+    substr_list = substr_list
+    if (not isinstance(substr_list, list)) & (not isinstance(substr_list, tuple)):
+        print('filter is not list')
+        return False
+
+    def filterfun(x):
+        for s in substr_list:
+            if s in str(x):
+                return True
+        return False
+
+    return filterfun
+
+
+def make_table(df, title='', align={}):
+    x = Ptt()
+    j = 0
+    for f in df.columns:
+        x.add_column(f, [x for x in df[f]])
+        if (f in align):
+            if (align[f] in ['l', 'c', 'r']):
+                x.align[f] = align[f]
+            elif df[f]._is_numeric_mixed_type:
+                x.align[f] = 'r'
+            elif df[f]._is_mixed_type:
+                x.align[f] = 'l'
+            else:
+                x.align[f] = 'c'
+        j = j + 1
+    rs = x.get_string()
+    return title.center(rs.index('\n')) + '\n' + rs
+
+
+def make_page(df, title='', pagelines=30, align={}):
+    gridnum = len(df.columns)
+    result = ''
+    ptext = make_table(df=df, title=title, align=align)
+    plist = ptext.split('\n')
+    # print(plist)
+    plen = len(plist)
+    hline = 0
+    textline = 0
+    head = ''
+    gapline = None
+    pagewid = 0
+    pageno = 0
+    for i in range(plen):
+        if hline < 2:
+            # set subtitle in center
+            if ('+' not in plist[i]) & (plist[i].count('|') == gridnum + 1):
+                sp = plist[i].split('|')
+                newsp = []
+                for x in sp:
+                    if len(x.strip()) < len(x):
+                        left_space = int((len(x) - len(x.strip()))/2)
+                        newsp.append(' '*left_space + x.strip() + ' '*(len(x) - left_space-len(x.strip())))
+                head += '|' + '|'.join(newsp) + '|\n'
+            else:
+                head += plist[i] + '\n'
+        else:
+            # not save first head in result
+            if i < plen -1:
+                result += plist[i] + '\n'
+        # find gapline and the end of head
+        if plist[i].count('+') == gridnum + 1:
+            hline = hline + 1
+            if gapline is None:
+                pagewid = len(plist[i])
+                gapline = plist[i] + '\n'
+            continue
+        # add first head+gapline in result
+        if (len(result) == 0) & (gapline is not None):
+            result = head + gapline
+        # start count content row number(textline)
+        if hline == 2:
+            textline += 1
+        # seperate pages
+        if (textline == pagelines) | (i == plen-2):
+            pageno += 1
+            pagenostr = ('--'+str(pageno)+'--').center(pagewid) + '\n\n'
+            result += gapline + pagenostr + (head if i < plen-2 else '')
+            textline = 0
+    return result
