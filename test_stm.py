3d563580ca6b357fe61f64531733f5da43cb3488
wangxichang <wangxichang@126.com>
2019-10-08 22:30:14 +0800

    modify test_stm.TestModel

--- test_stm.py
+++ test_stm.py
@@ -5,18 +5,18 @@ import numpy as np
 import pyex_stm as stm
 import importlib as pb
 
-class Score():
+class TestModel():
 
     def __init__(self):
-        self.df17li = pd.read_csv('d:/mywrite/newgk/gkdata/17/like.csv', sep=',',
+        self.df17like = pd.read_csv('d:/mywrite/newgk/gkdata/17/like.csv', sep=',',
                                   usecols=('wl', 'hx', 'sw'))
         self.df17wen = pd.read_csv('d:/mywrite/newgk/gkdata/17/wenke.csv', sep=',',
                                   usecols=('zz', 'ls', 'dl'))
-        self.df18li = pd.read_csv('d:/mywrite/newgk/gkdata/18/like.csv', sep=',',
+        self.df18like = pd.read_csv('d:/mywrite/newgk/gkdata/18/like.csv', sep=',',
                                   usecols=('wl', 'hx', 'sw'))
         self.df18wen = pd.read_csv('d:/mywrite/newgk/gkdata/18/wenke.csv', sep=',',
                                    usecols=('zz', 'ls', 'dl'))
-        self.m = None
+        self.model_dict = dict()
 
     def run_stm(self,
                 name='shandong',
@@ -27,12 +27,14 @@ class Score():
                 mode_score_order='d'
                 ):
         pb.reload(stm)
-        dfs = {'17like': self.df17li, '17wenke': self.df17wen,
-               '18like': self.df18li, '18wenke': self.df18wen}
-        self.m = stm.run_stm(
-            name=name, df=dfs[year+kl],
-            field_list=list(dfs[year+kl]),
+        dfs = {'17like': self.df17like, '17wenke': self.df17wen,
+               '18like': self.df18like, '18wenke': self.df18wen}
+        m = stm.run_stm(
+            name=name,
+            df=dfs[year+kl],
+            fs=list(dfs[year+kl]),
             mode_ratio_approx=mode_ratio_approx,
             mode_ratio_cumu=mode_ratio_cumu,
             mode_score_order=mode_score_order
             )
+        self.model_dict.update({year+'_'+kl+'_'+mode_ratio_approx+'_'+mode_ratio_cumu: m})
