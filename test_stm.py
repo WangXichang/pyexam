f41161144391a5111d1d0c52b1dc57960ba1efea
wangxichang <wangxichang@126.com>
2019-10-10 20:35:45 +0800

    T,Zscore modify

--- test_stm.py
+++ test_stm.py
@@ -8,12 +8,22 @@ import importlib as pb
 class TestModel():
 
     def __init__(self):
+        self.df16like = pd.read_csv('d:/mywrite/newgk/gkdata/17/like.csv', sep=',',
+                                  usecols=('wl', 'hx', 'sw'))
+        self.df16like.wl = self.df16like.wl.apply(lambda x: int(x*10/11))
+        self.df16like.sw = self.df16like.sw.apply(lambda x: int(x*10/9))
+        self.df16wen = pd.read_csv('d:/mywrite/newgk/gkdata/17/wenke.csv', sep=',',
+                                  usecols=('zz', 'ls', 'dl'))
         self.df17like = pd.read_csv('d:/mywrite/newgk/gkdata/17/like.csv', sep=',',
                                   usecols=('wl', 'hx', 'sw'))
+        self.df17like.wl = self.df17like.wl.apply(lambda x: int(x*10/11))
+        self.df17like.sw = self.df17like.sw.apply(lambda x: int(x*10/9))
         self.df17wen = pd.read_csv('d:/mywrite/newgk/gkdata/17/wenke.csv', sep=',',
                                   usecols=('zz', 'ls', 'dl'))
         self.df18like = pd.read_csv('d:/mywrite/newgk/gkdata/18/like.csv', sep=',',
                                   usecols=('wl', 'hx', 'sw'))
+        self.df18like.wl = self.df18like.wl.apply(lambda x: int(x*10/11))
+        self.df18like.sw = self.df18like.sw.apply(lambda x: int(x*10/9))
         self.df18wen = pd.read_csv('d:/mywrite/newgk/gkdata/18/wenke.csv', sep=',',
                                    usecols=('zz', 'ls', 'dl'))
         self.model_dict = dict()
@@ -23,12 +33,17 @@ class TestModel():
                 year='17',
                 kl='like',
                 mode_ratio_approx='upper_min',
-                mode_ratio_cumu='yes',
+                mode_ratio_cumu='no',
                 mode_score_order='d'
                 ):
         pb.reload(stm)
-        dfs = {'17like': self.df17like, '17wenke': self.df17wen,
-               '18like': self.df18like, '18wenke': self.df18wen}
+        dfs = {
+            '16like': self.df16like,
+            '16wenke': self.df16wen,
+            '17like': self.df17like,
+            '17wenke': self.df17wen,
+            '18like': self.df18like,
+            '18wenke': self.df18wen}
         m = stm.run_stm(
             name=name,
             df=dfs[year+kl],
@@ -37,4 +52,4 @@ class TestModel():
             mode_ratio_cum=mode_ratio_cumu,
             mode_score_order=mode_score_order
             )
-        self.model_dict.update({year+'_'+kl+'_'+mode_ratio_approx+'_'+mode_ratio_cumu: m})
+        self.model_dict.update({name+'_'+year+'_'+kl+'_'+mode_ratio_approx+'_'+mode_ratio_cumu: m})
