73d24651a9c91c5d9fb6be3e3a41a0a928997e84
wangxichang <wangxichang@126.com>
2019-12-22 12:43:47 +0800

    add modelapi

--- stm/modeltest.py
+++ stm/modeltest.py
@@ -41,92 +41,97 @@ def time_disper(fun):
     return dec_fun
 
 
-def data_lv():
-    file_path = 'f:/mywrite/新高考改革/modelstestdata/testdata/'
-    cumu_file_list = [file_path+'cumu/'+str(p)+'/out18wl_stand.csv' for p in range(1, 10)]
-    nocumu_file_dict = dict()
-    data_cumu = dict()
-    data_nocumu = dict()
-    for i, fname in enumerate(cumu_file_list):
-        if os.path.isfile(fname):
-            print('load: '+ fname)
-        else:
-            print('fail:' + fname)
-            continue
-        with open(fname) as f:
-            data_cumu.update({'cumu'+str(i): pd.read_csv(f)})
-    nocumu_file_dict.update({'test1': file_path+'nocumu/test1/结果/out2018lz_stand.csv'})
-    nocumu_file_dict.update({'test2': file_path+'nocumu/test2/结果/out_stand.csv'})
-    nocumu_file_dict.update({'test3': file_path+'nocumu/test3/结果/out2018wl_stand.csv'})
+class TestLvData():
 
-    nocumu_file_dict.update({'test4hx': file_path+'nocumu/test4/结果/out18hx_stand.csv'})
-    # test4hx-- find error: seek 0.5 at 0.50000, lv-wang: at 0.509924
-    nocumu_file_dict.update({'test4wl': file_path+'nocumu/test4/结果/out18wl_stand.csv'})
+    def __init__(self):
+        self.data = None
+        self.result = None
 
-    nocumu_file_dict.update({'test5sw': file_path+'nocumu/test5/结果/out18sw_stand.csv'})
-    # test5sw-- zero-max: [0, 0] => [21, 30]
-    nocumu_file_dict.update({'test5hx': file_path+'nocumu/test5/结果/out18hx_stand.csv'})
-    nocumu_file_dict.update({'test5wl': file_path+'nocumu/test5/结果/out18wl_stand.csv'})
+    def load_data(self):
+        file_path = 'f:/mywrite/新高考改革/modelstestdata/testdata/'
+        cumu_file_list = [file_path+'cumu/'+str(p)+'/out18wl_stand.csv' for p in range(1, 10)]
+        nocumu_file_dict = dict()
+        data_cumu = dict()
+        data_nocumu = dict()
+        for i, fname in enumerate(cumu_file_list):
+            if os.path.isfile(fname):
+                print('load: '+ fname)
+            else:
+                print('fail:' + fname)
+                continue
+            with open(fname) as f:
+                data_cumu.update({'cumu'+str(i): pd.read_csv(f)})
+        nocumu_file_dict.update({'test1': file_path+'nocumu/test1/结果/out2018lz_stand.csv'})
+        nocumu_file_dict.update({'test2': file_path+'nocumu/test2/结果/out_stand.csv'})
+        nocumu_file_dict.update({'test3': file_path+'nocumu/test3/结果/out2018wl_stand.csv'})
 
-    nocumu_file_dict.update({'test6hx': file_path+'nocumu/test6/结果/out2018hx_stand.csv'})
+        nocumu_file_dict.update({'test4hx': file_path+'nocumu/test4/结果/out18hx_stand.csv'})
+        # test4hx-- find error: seek 0.5 at 0.50000, lv-wang: at 0.509924
+        nocumu_file_dict.update({'test4wl': file_path+'nocumu/test4/结果/out18wl_stand.csv'})
 
-    nocumu_file_dict.update({'test7wl18': file_path+'nocumu/test7/结果/out18wl_stand.csv'})
-    nocumu_file_dict.update({'test7wl19': file_path+'nocumu/test7/结果/out19wl_stand.csv'})
+        nocumu_file_dict.update({'test5sw': file_path+'nocumu/test5/结果/out18sw_stand.csv'})
+        # test5sw-- zero-max: [0, 0] => [21, 30]
+        nocumu_file_dict.update({'test5hx': file_path+'nocumu/test5/结果/out18hx_stand.csv'})
+        nocumu_file_dict.update({'test5wl': file_path+'nocumu/test5/结果/out18wl_stand.csv'})
 
-    nocumu_file_dict.update({'test8': file_path+'nocumu/test8/out18wl_stand.csv'})
+        nocumu_file_dict.update({'test6hx': file_path+'nocumu/test6/结果/out2018hx_stand.csv'})
 
-    for dname in nocumu_file_dict:
-        if os.path.isfile(nocumu_file_dict[dname]):
-            print('load: '+dname)
-        else:
-            print('fail: '+nocumu_file_dict[dname])
-            continue
-        with open(nocumu_file_dict[dname]) as f_out:
-            data_nocumu.update({dname: pd.read_csv(f_out)})
+        nocumu_file_dict.update({'test7wl18': file_path+'nocumu/test7/结果/out18wl_stand.csv'})
+        nocumu_file_dict.update({'test7wl19': file_path+'nocumu/test7/结果/out19wl_stand.csv'})
 
-    # merge test4
-    # data_nocumu.update({'test4': pd.merge(data_nocumu['test4wl'], data_nocumu['test4hx'], on='ksh')})
-    # data_nocumu.pop('test4wl')
-    # data_nocumu.pop('test4hx')
-    #
-    # # merge test5
-    # df = None
-    # for sub in ['sw', 'hx', 'wl']:
-    #     if sub == 'sw':
-    #         df = data_nocumu['test5wl'].__deepcopy__()
-    #     else:
-    #         df = pd.merge(df, data_nocumu['test5'+sub], on='ksh')
-    #     data_nocumu.pop('test5'+sub)
-    # data_nocumu.update({'test5': df})
+        nocumu_file_dict.update({'test8': file_path+'nocumu/test8/out18wl_stand.csv'})
 
-    data_cumu.update(data_nocumu)
+        for dname in nocumu_file_dict:
+            if os.path.isfile(nocumu_file_dict[dname]):
+                print('load: '+dname)
+            else:
+                print('fail: '+nocumu_file_dict[dname])
+                continue
+            with open(nocumu_file_dict[dname]) as f_out:
+                data_nocumu.update({dname: pd.read_csv(f_out)})
 
-    return data_cumu
+        # merge test4
+        # data_nocumu.update({'test4': pd.merge(data_nocumu['test4wl'], data_nocumu['test4hx'], on='ksh')})
+        # data_nocumu.pop('test4wl')
+        # data_nocumu.pop('test4hx')
+        #
+        # # merge test5
+        # df = None
+        # for sub in ['sw', 'hx', 'wl']:
+        #     if sub == 'sw':
+        #         df = data_nocumu['test5wl'].__deepcopy__()
+        #     else:
+        #         df = pd.merge(df, data_nocumu['test5'+sub], on='ksh')
+        #     data_nocumu.pop('test5'+sub)
+        # data_nocumu.update({'test5': df})
 
+        data_cumu.update(data_nocumu)
 
-@time_disper
-def test_lv(data):
-    r_dict = dict()
-    for num in range(9):
-        r = test_stm_with_lvdata(data=data['cumu'+str(num)], cols=['wl'], cumu='yes', name='cumu_'+str(num))
-        r_dict.update({'cumu'+str(num): r[0]})
-    nocumu_names = ['test'+str(i) for i in range(1, 4)] + \
-                   ['test4wl', 'test4hx', 'test5wl', 'test5hx', 'test5sw', 'test6hx', 'test7wl18', 'test7wl19', 'test8']
-    for num in range(len(nocumu_names)):
-        r = test_stm_with_lvdata(data=data[nocumu_names[num]],
-                                 cols=('wl', 'hx', 'sw'), cumu='no', name=nocumu_names[num])
-        r_dict.update({nocumu_names[num]: r[0]})
-    return r_dict
+        self.data = data_cumu
 
+    @time_disper
+    def test(self, data):
+        r_dict = dict()
+        for num in range(9):
+            r = self.test_stm_with_lvdata(data=data['cumu'+str(num)], cols=['wl'], cumu='yes', name='cumu_'+str(num))
+            r_dict.update({'cumu'+str(num): r[0]})
+        nocumu_names = ['test'+str(i) for i in range(1, 4)] + \
+                       ['test4wl', 'test4hx', 'test5wl', 'test5hx', 'test5sw', 'test6hx', 'test7wl18', 'test7wl19', 'test8']
+        for num in range(len(nocumu_names)):
+            r = self.test_stm_with_lvdata(data=data[nocumu_names[num]],
+                                     cols=('wl', 'hx', 'sw'), cumu='no', name=nocumu_names[num])
+            r_dict.update({nocumu_names[num]: r[0]})
+        self.result =  r_dict
 
-def test_stm_with_lvdata(data=None, cols=('wl', 'hx', 'sw'), cumu='no', name=''):
-    cols_real = [f for f in cols if f in data.columns]
-    mr = model.run(df=data, cols=cols_real, mode_ratio_cumu=cumu)
-    mr.save_report_to_file('f:/mywrite/新高考改革/modelstestdata/testdata/report_'+name+'.txt')
-    result = []
-    for col in cols_real:
-        result.append([col, len(mr.out_data.query(col+'_plt !='+col+'_stand')) == 0])
-    return result, mr
+    def test_stm_with_lvdata(self, data=None, cols=('wl', 'hx', 'sw'), cumu='no', name=''):
+        cols_real = [f for f in cols if f in data.columns]
+        mr = model.run(df=data, cols=cols_real, mode_ratio_cumu=cumu)
+        mr.save_report_to_file('f:/mywrite/新高考改革/modelstestdata/testdata/report_'+name+'.txt')
+        result = []
+        for col in cols_real:
+            result.append([col, len(mr.out_data.query(col+'_plt !='+col+'_stand')) == 0])
+        self.result = result
+        self.result_model = mr
 
 
 @time_disper
@@ -145,7 +150,7 @@ def test_hainan(mean=60, size=60000, std=16):
     return result
 
 
-class TestStmWithShandongData():
+class TestShandongData():
 
     def __init__(self):
         self.df16like = pd.read_csv('e:/mywrite/newgk/gkdata/17/like.csv', sep=',',
@@ -175,7 +180,7 @@ class TestStmWithShandongData():
                 kl='wenke',
                 mode_ratio_prox='upper_min',
                 mode_ratio_cumu='no',
-                mode_ratio_sort='d',
+                mode_sort_order='d',
                 all='no'
                 ):
         pb.reload(model)
@@ -197,7 +202,7 @@ class TestStmWithShandongData():
                 cols=list(dfs[_run[0]+_run[1]]),
                 mode_ratio_prox=mode_ratio_prox,
                 mode_ratio_cumu=mode_ratio_cumu,
-                mode_ratio_sort=mode_ratio_sort
+                mode_sort_order=mode_sort_order
                 )
             self.models_list.append(
                 self.model(name + '_' + _run[0] + '_' + _run[1] + '_' + mode_ratio_prox + '_' + mode_ratio_cumu, m))
@@ -302,7 +307,7 @@ class TestData:
 
 
 # create normal distributed data N(mean,std), [-std*stdNum, std*stdNum], sample points = size
-def get_norm_table2(size=400, std=1, mean=0, stdnum=4):
+def get_norm_table(size=400, std=1, mean=0, stdnum=4):
     """
     function
         生成正态分布量表
