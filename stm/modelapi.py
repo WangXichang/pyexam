9033b6ffd0d2d44b966e2b83e50c3d5c36b353b1
wangxichang <wangxichang@126.com>
2019-12-23 17:51:54 +0800

    add get_stm_score

--- stm/modelapi.py
+++ stm/modelapi.py
@@ -63,95 +63,6 @@ def use_ellipsis_in_digits_seq(digit_seq):
 
 
 class ModelAlgorithm:
-    @classmethod
-    def plot_models(cls, font_size=12, hainan='900'):
-        _names = ['shanghai', 'zhejiang', 'beijing', 'tianjin', 'shandong', 'guangdong', 'ss7', 'hn900']
-        if hainan == '300':
-            _names.remove('hn900')
-            _names.append('hn300')
-        elif hainan is None:
-            _names.remove('hn900')
-        ms_dict = dict()
-        for _name in _names:
-            ms_dict.update({_name: ModelAlgorithm.get_model_describe(name=_name)})
-
-        plot.figure('New Gaokao Score Models: name(mean, std, skewness)')
-        plot.rcParams.update({'font.size': font_size})
-        for i, k in enumerate(_names):
-            plot.subplot(240+i+1)
-            _wid = 2
-            if k in ['shanghai']:
-                x_data = range(40, 71, 3)
-            elif k in ['zhejiang', 'beijing', 'tianjin']:
-                x_data = range(40, 101, 3)
-            elif k in ['shandong']:
-                x_data = [x for x in range(26, 100, 10)]
-                _wid = 8
-            elif k in ['guangdong']:
-                x_data = [np.mean(x) for x in mcf.Models[k].section][::-1]
-                _wid = 10
-            elif k in ['ss7']:
-                x_data = [np.mean(x) for x in mcf.Models[k].section][::-1]
-                _wid = 10
-            elif k in ['hn900']:
-                x_data = [x for x in range(100, 901)]
-                _wid = 1
-            elif k in ['hn300']:
-                x_data = [x for x in range(60, 301)]
-                _wid = 1
-            else:
-                raise ValueError(k)
-            plot.bar(x_data, mcf.Models[k].ratio[::-1], width=_wid)
-            plot.title(k+'({:.2f}, {:.2f}, {:.2f})'.format(*ms_dict[k]))
-
-    @classmethod
-    def add_model(cls, model_type='plt', name=None, ratio_list=None, section_list=None, desc=''):
-        if model_type not in mcf.MODEL_TYPE:
-            print('error model type={}, valid type:{}'.format(model_type, mcf.MODEL_TYPE))
-            return
-        if name in mcf.Models:
-            print('name existed in current models_dict!')
-            return
-        if len(ratio_list) != len(section_list):
-            print('ratio is not same as segment !')
-            return
-        for s in section_list:
-            if len(s) > 2:
-                print('segment is not 2 endpoints: {}-{}'.format(s[0], s[1]))
-                return
-            if s[0] < s[1]:
-                print('the order is from large to small: {}-{}'.format(s[0], s[1]))
-                return
-        if not all([s1 >= s2 for s1, s2 in zip(section_list[:-1], section_list[1:])]):
-            print('section endpoints order is not from large to small!')
-            return
-        mcf.Models.update({name: mcf.ModelFields(model_type,
-                                                 ratio_list,
-                                                 section_list,
-                                                 desc)})
-        MODELS_NAME_LIST = mcf.Models.keys()
-
-    @classmethod
-    def show_models(cls):
-        for k in mcf.Models:
-            v = mcf.Models[k]
-            print('{:<20s} {},  {} '.format(k, v.type, v.desc))
-            print('{:<20s} {}'.format(' ', v.ratio))
-            print('{:<20s} {}'.format('', v.section))
-
-    @classmethod
-    def get_model_describe(cls, name='shandong'):
-        __ratio = mcf.Models[name].ratio
-        __section = mcf.Models[name].section
-        if name == 'hn900':
-            __mean, __std, __skewness = 500, 100, 0
-        elif name == 'hn300':
-            __mean, __std, __skewness = 180, 30, 0
-        else:
-            samples = []
-            [samples.extend([np.mean(s)]*int(__ratio[i])) for i, s in enumerate(__section)]
-            __mean, __std, __skewness = np.mean(samples), np.std(samples), sts.skew(np.array(samples))
-        return __mean, __std, __skewness
 
     @classmethod
     def get_section_pdf(cls,
@@ -602,7 +513,7 @@ class ModelAlgorithm:
                       mode_section_point_last='real',
                       mode_section_degraded='map_to_max',
                       mode_section_lost='ignore',
-                      out_score_decimal=2,
+                      out_score_decimal=0,
                       ):
         seg = run_seg(indf=df,
                       cols=cols,
--- stm/modelconfig.py
+++ stm/modelconfig.py
@@ -56,6 +56,8 @@
 
 from collections import namedtuple
 import scipy.stats as sts
+import numpy as np
+import matplotlib.pyplot as plot
 from stm import modelapi as mapi
 
 # model type
@@ -163,3 +165,94 @@ Strategies = {
 #       'mode_score_rmax_to_max': ('ignore', 'yes'),  # real raw score max value to out score max value,
 #                                                     # case: top ratio large, sort by 'a', standard score mode
 # }
+
+
+class ModelStats:
+    @classmethod
+    def plot_models(cls, font_size=12, hainan='900'):
+        _names = ['shanghai', 'zhejiang', 'beijing', 'tianjin', 'shandong', 'guangdong', 'ss7', 'hn900']
+        if hainan == '300':
+            _names.remove('hn900')
+            _names.append('hn300')
+        elif hainan is None:
+            _names.remove('hn900')
+        ms_dict = dict()
+        for _name in _names:
+            ms_dict.update({_name: mapi.ModelAlgorithm.get_model_describe(name=_name)})
+
+        plot.figure('New Gaokao Score Models: name(mean, std, skewness)')
+        plot.rcParams.update({'font.size': font_size})
+        for i, k in enumerate(_names):
+            plot.subplot(240+i+1)
+            _wid = 2
+            if k in ['shanghai']:
+                x_data = range(40, 71, 3)
+            elif k in ['zhejiang', 'beijing', 'tianjin']:
+                x_data = range(40, 101, 3)
+            elif k in ['shandong']:
+                x_data = [x for x in range(26, 100, 10)]
+                _wid = 8
+            elif k in ['guangdong']:
+                x_data = [np.mean(x) for x in Models[k].section][::-1]
+                _wid = 10
+            elif k in ['ss7']:
+                x_data = [np.mean(x) for x in Models[k].section][::-1]
+                _wid = 10
+            elif k in ['hn900']:
+                x_data = [x for x in range(100, 901)]
+                _wid = 1
+            elif k in ['hn300']:
+                x_data = [x for x in range(60, 301)]
+                _wid = 1
+            else:
+                raise ValueError(k)
+            plot.bar(x_data, Models[k].ratio[::-1], width=_wid)
+            plot.title(k+'({:.2f}, {:.2f}, {:.2f})'.format(*ms_dict[k]))
+
+    @classmethod
+    def add_model(cls, model_type='plt', name=None, ratio_list=None, section_list=None, desc=''):
+        if model_type not in ['ppt', 'plt']:
+            print('error model type={}, valid type:{}'.format(model_type, ['ppt', 'plt']))
+            return
+        if name in Models:
+            print('name existed in current models_dict!')
+            return
+        if len(ratio_list) != len(section_list):
+            print('ratio is not same as segment !')
+            return
+        for s in section_list:
+            if len(s) > 2:
+                print('segment is not 2 endpoints: {}-{}'.format(s[0], s[1]))
+                return
+            if s[0] < s[1]:
+                print('the order is from large to small: {}-{}'.format(s[0], s[1]))
+                return
+        if not all([s1 >= s2 for s1, s2 in zip(section_list[:-1], section_list[1:])]):
+            print('section endpoints order is not from large to small!')
+            return
+        Models.update({name: ModelFields(model_type,
+                                                 ratio_list,
+                                                 section_list,
+                                                 desc)})
+
+    @classmethod
+    def show_models(cls):
+        for k in Models:
+            v = Models[k]
+            print('{:<20s} {},  {} '.format(k, v.type, v.desc))
+            print('{:<20s} {}'.format(' ', v.ratio))
+            print('{:<20s} {}'.format('', v.section))
+
+    @classmethod
+    def get_model_describe(cls, name='shandong'):
+        __ratio = Models[name].ratio
+        __section = Models[name].section
+        if name == 'hn900':
+            __mean, __std, __skewness = 500, 100, 0
+        elif name == 'hn300':
+            __mean, __std, __skewness = 180, 30, 0
+        else:
+            samples = []
+            [samples.extend([np.mean(s)]*int(__ratio[i])) for i, s in enumerate(__section)]
+            __mean, __std, __skewness = np.mean(samples), np.std(samples), sts.skew(np.array(samples))
+        return __mean, __std, __skewness
