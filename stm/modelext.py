bebe6f0f66fe73dc76992f6a68079f2e4e53dea6
wangxichang <wangxichang@126.com>
2019-12-26 22:01:29 +0800

    call run_model in main.run

--- stm/modelext.py
+++ stm/modelext.py
@@ -8,8 +8,8 @@ ModelFields = namedtuple('ModelFields', ['type', 'ratio', 'section', 'desc'])
 Models_ext={
     'exp':          ModelFields(
                                 'plt',
-                                [ 1 /15 *100 for _ in range(15)],     # only first==1 is useful
-                                tuple(( i +1, i+ 1) for i in range(15)),
-                                'piecewise linear transform model with ratio-segment'
+                                [1/15 *100 for _ in range(15)],     # only first==1 is useful
+                                tuple((i+1, i+1) for i in range(15)),
+                                'piecewise linear transform model with ratio-section'
                                 ),
     }
