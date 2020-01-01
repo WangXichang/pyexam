c6192041a342333255c936ca09f5dff89e6d55c2
wangxichang <wangxichang@126.com>
2020-01-01 21:43:17 +0800

    fix error sort_order in get_stm_score for ppt

--- stm/models_ext.py
+++ stm/models_ext.py
@@ -10,19 +10,20 @@
     设置在Models_ext中的模型，在运行main.run时， 可以通过设置reload=True，使系统自动加入到Models中。
     MODEL = {name: ModelFields}
     ModelFields: {
-                  'type':       str := 'plt' or 'ppt',
+                  'type':       str, model type: 'plt'，'ppt', ‘pgt'
                   'ratio':      list or tuple:= percent value for each section,
-                                100 percent: 0-100,
-                                descending: from big section to small, corresponding to the position of section
-                                i.e. (1, 2, ..., 1), sum(ratio)==100
+                                item range: 0-100
+                                order: corresponding to the position of section
+                                sum(ratio) == 100
                   'section':    list or tuple:= output score section,
-                                descending: from big score to min score
+                                order: descending, big to small, max score to min score
+                                range: section_first_point=max out score, section_last_point=min out score
                                 i.e. [(p11, p12), ...(pn1, pn2)], pi1 < pi2, and, pji < pk1 for k>j
-                  'desc':       str := description for model
+                  'desc':       str := description for the model
                   }
     （2）参数描述
      1）name
-    模型名称，尽量避免与 modelsetin.Models 中的已有名称重复, 否则会覆盖内置的模型;
+    模型名称，应避免与 models_in.Models 中的已有名称重复, 否则会覆盖内置的模型;
     可以通过查看modelsetin 或 使用 name in modelsetin.Models.keys() 进行检查。
     使用main.run(..., reload=True)时会检查提示。
      2）type
@@ -39,14 +40,16 @@
     在pgt中，用第一个比例给出高分区，通过计算高分区均值给出第一个区间的第二端点。其余比例不使用。
 
      4）section
-    转换分数区间，个数与比例个数相同。
-    端点顺序从大到小，即从高分到低分
-    在plt中，用于将原始分数的每个区间映射到section区间，端点根据标准确定。
-    在ppt中，用于将原始分数的每个分值映射到section端点，ppt的区间端点值是相等的，为本分值。
-    在pgt中，用于将原始分数等级区间值映射到section端点, pgt的区间端点值是相同的，为本等级值。
+    转换分数区间，个数与比例个数相同, 区间应覆盖分数的范围。
+    端点顺序从大到小，即从高分到低分，如：[100, 91], [90, 81], ..., [10,0]
 
-    注1：如果在本模块中修改增加了模型，使用main.run时需要设置reload=True，重新载入Models_ext
-    注2：在使用 main.run 或 main.run_model 调用新定义的模型前，可以使用modelutil.check_model检查是否存在设置问题。
+    在plt中，section为转换分数区间，用于将原始分数的等级区间映射到section区间。
+    在ppt中，section为转换分数分值，用于将原始分数的每个分值映射到section端点，ppt的区间端点值是相等的，为对应转换分值。
+    在pgt中，section为等级分数分值，用于将原始分数的等级区间映射到section端点, pgt的区间端点值是相同的，为对应等级值。
+
+    注1：在models_ext中修改或增加了模型，再调用main.run时，需要设置 reload=True，重新载入Models_ext。
+    注2：在models_ext中定义了新定义的模型前，可以使用modelutil.check_model检查是否存在设置问题。
+
 """
 
 
