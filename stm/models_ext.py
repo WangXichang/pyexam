a5e707f717454fab67204ba58cc72823830fe74e
wangxichang <wangxichang@126.com>
2020-01-01 20:58:36 +0800

    fix error sort_order in get_stm_score for ppt

--- stm/models_ext.py
+++ stm/models_ext.py
@@ -12,9 +12,12 @@
     ModelFields: {
                   'type':       str := 'plt' or 'ppt',
                   'ratio':      list or tuple:= percent value for each section,
-                                i.e. (1, 2, ..., 1), sum==100
+                                100 percent: 0-100,
+                                descending: from big section to small, corresponding to the position of section
+                                i.e. (1, 2, ..., 1), sum(ratio)==100
                   'section':    list or tuple:= output score section,
-                                i.e. [(p11, p12), ...(pn1, pn2)], pi1 > pi2
+                                descending: from big score to min score
+                                i.e. [(p11, p12), ...(pn1, pn2)], pi1 < pi2, and, pji < pk1 for k>j
                   'desc':       str := description for model
                   }
     （2）参数描述
@@ -27,13 +30,17 @@
     'ppt'     # 逐点转换 piecewise point transform
     
      3) ratio
-    每个分数区间的比例，个数需要与区间数相同
+    每个分数区间的考生人数占比，
+    每个比例对应与section中的区间：ratio[i] --> section[i]
+    比例数值的个数需要与区间数相同： len(ratio) == len(section)
+    比例和为100：sum(ratio) == 100
     在plt中，用于定义原始分数划分区间，
     在ppt中，用于定义转换分数的分值点比例
     在pgt中，用第一个比例给出高分区，通过计算高分区均值给出第一个区间的第二端点。其余比例不使用。
 
      4）section
     转换分数区间，个数与比例个数相同。
+    端点顺序从大到小，即从高分到低分
     在plt中，用于将原始分数的每个区间映射到section区间，端点根据标准确定。
     在ppt中，用于将原始分数的每个分值映射到section端点，ppt的区间端点值是相等的，为本分值。
     在pgt中，用于将原始分数等级区间值映射到section端点, pgt的区间端点值是相同的，为本等级值。
